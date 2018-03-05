d30c0549fc3a032953eb304accf88a0943c77545
ChiakiChiaki <779856065@qq.com>
2018-03-06 03:57:00 +0800

    update

--- Math/9. Palindrome Number.py
+++ Math/9. Palindrome Number.py
@@ -1,2 +1,25 @@
 #Author:Sun Jian
 
+'''
+Determine whether an integer is a palindrome. Do this without extra space.
+
+'''
+
+
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0 or (x%10 ==0 and x!=0 ):
+            return False
+
+        else:
+            s=0
+            p=x
+            while p>0:
+                s=s*10+p%10
+                p//=10
+            return s==x
+
